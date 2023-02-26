332b8e3de8355bf831616cd43556db2f863ddd12
MelnikovSG <melnikov-sergey-g@mail.ru>
2023-02-26 20:40:01 +0700

    Created a bot added a discord to the server created a JDA with a bot token

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class App {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class App {
+    public static void main(String[] args) {
+        JDA jda = JDABuilder.createDefault
+                ("MTA3OTM2NTYxODgyODA3MTAxMw.GPCtBS.3nQDg0D37ggMrmkSP-BN5cTUBi2r4aVD3Tvud8")
+                .build();
+
+    }
 }
